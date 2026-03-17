b6fa6abb8607c2e4e2dede4f7293fec1758a2670
shamil.khedgikar <shamil.khedgikar@aecom.com>
2026-03-17 15:42:42 -0400

    removed fulldev-ui reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,8 @@
 import { defineConfig } from 'astro/config'
-import fulldevUi from 'fulldev-ui/integration'
 import mdx from '@astrojs/mdx'   // add this
 
 export default defineConfig({
   integrations: [
     mdx(),         // safe even if you don't have .mdx files yet
-    fulldevUi({})
   ],
-  // TEMPORARY shim for any deps still importing "astro/jsx/server.js"
-  vite: {
-    resolve: {
-      alias: {
-        'astro/jsx/server.js': '@astrojs/mdx/server.js'
-      }
-    }
-  }
 })
